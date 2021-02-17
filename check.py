68246f1f2e04bd574f42de937d232d3e059c5883
abulavin <artemy@live.co.uk>
2021-02-17 10:57:08 +0000

    Create script to restart apache if HTTP request unsuccessful

---
+++ check.py
@@ -0,0 +1,37 @@
+import os
+import logging
+import subprocess
+import requests
+import schedule
+import time
+from configparser import ConfigParser
+
+parser = ConfigParser()
+parser.read("./config.ini")    
+
+def restart_apache():
+    logging.info("Restarting apache")
+    command = "/usr/sbin/httpd -d $HOME/apache -k restart"
+    command_expanded = os.path.expandvars(command)
+    subprocess.run(command_expanded)
+
+
+def send_request():
+    url = parser['url']['url']
+    logging.info(f"Checking HTTP status at: {url}")
+    response = requests.get(url)
+    logging.info(f"Received status code {response.status_code}")
+    if not response.ok:
+        restart_apache()
+
+
+if __name__ == "__main__":
+    logging.basicConfig(
+        format='%(asctime)s [%(levelname)s] %(message)s',
+        level=logging.INFO,
+        datefmt='%Y-%m-%d %H:%M:%S'
+    )
+    schedule.every().hour.do(send_request)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
